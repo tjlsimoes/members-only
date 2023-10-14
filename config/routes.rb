921f6347e6dcfef7c0b38485dbe7198ad072ef93
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-14 12:42:48 +0100

    Enable sign_out through GET request

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	resources :posts, only: [:index, :new, :create]
-	devise_for :users, controllers: { registrations: 'users/registrations' }
+	devise_for :users, controllers: { registrations: 'users/registrations' }, sign_out_via: [:get, :post]
 
-  # root "posts#index"
+  root "posts#index"
 end
